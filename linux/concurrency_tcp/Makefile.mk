6c872e11a7e7108453b1ed08bef8e3e56b05bde2
zhaoaquei <zhaoaquei@863c8ad8-5013-11de-a7b9-cdf4705868f5>
2010-06-08 14:12:28 +0000

    add comment

--- linux/concurrency_tcp/Makefile.mk
+++ linux/concurrency_tcp/Makefile.mk
@@ -1,4 +1,15 @@
-#nmake all /f Makefile.mk
+###########################################
+#@file		
+#@brief	
+#@details
+#	usage:
+#		nmake all /f Makefile.mk
+#		nmake clean /f Makefile.mk
+#@author	zuohaitao
+#@date		2010-06-08 ÐÇÆÚ¶þ 
+#@warning	
+#@bug		
+###########################################
 all:
 	cl concurrency_server.c /Zi  /DWIN32
 	cl concurrency_client.c /Zi /DWIN32
