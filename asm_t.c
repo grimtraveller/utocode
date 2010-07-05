6a29fa69e48e88f988f2f2153cea400c49b2176e
zhaoaquei <zhaoaquei@863c8ad8-5013-11de-a7b9-cdf4705868f5>
2010-07-05 14:07:19 +0000

    it is ported in win32

--- asm_t.c
+++ asm_t.c
@@ -4,16 +4,27 @@
  * Usage:	$>gcc src/asm_t.c -ggdb -Wall -o bin/asm_t
  *			$>gdb bin/asm_t
  *			(gdb)dissas main
+ *			>cl asm_t.c -D "WIN32"
+ *			>asm_t.exe
  * Author:	zuohaitao
  * Date:	2009-07-02
  */
 
 int main()
 {
+#ifdef WIN32
+	__asm
+	{
+		int3
+		nop
+		nop
+	}
+#else /* linux */
 	__asm__
 	(
 	"nop\n\t"
 	"nop\n\t"
 	);
+#endif //WIN32
 	return 1;
 }
