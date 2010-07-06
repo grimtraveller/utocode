2bf9a4ca466bcac5f4214e336ef907a64c7dcabb
zhaoaquei <zhaoaquei@863c8ad8-5013-11de-a7b9-cdf4705868f5>
2010-07-06 17:26:56 +0000

    add scape character

--- ascii.c
+++ ascii.c
@@ -4,6 +4,10 @@
  * Usage:	$>ascii a b c
  * Author:	zuohaitao
  * Date:	2009-03-10
+ * Modify:
+ *		what:	scape char convert
+ *		who:	zuohaitao
+ *		date:	2010-07-07
  */
 #include <stdio.h>
 int main(int argc,char* argv[])
@@ -11,13 +15,24 @@ int main(int argc,char* argv[])
 	int i;
 	if (argc < 2)
 	{
-		printf("usag: ascii character\n");
+		printf("usage: ascii character\n");
 		return 1;
 	}
 
 	for (i = 1; i < argc; i++)
 	{
-		printf("%c: 0x%x %d\n", argv[i][0], argv[i][0], argv[i][0]);
+
+		if (0 == strcmp("\'\\n\'", argv[i]))
+		{
+			printf("%s:\t0x%02x %03d\n", "\'\\n\'", '\n', '\n');
+			continue;
+		}
+		if (0 == strcmp("\'\\b\'", argv[i]))
+		{
+			printf("%s:\t0x%02x %03d\n", "\'\\b\'", '\b', '\b');
+			continue;
+		}
+		printf("\'%c\':\t0x%02x %03d\n", argv[i][0], argv[i][0], argv[i][0]);
 	}
 	return 0;
 }
